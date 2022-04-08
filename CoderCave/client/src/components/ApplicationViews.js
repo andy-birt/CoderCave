af456b2ebcd235f269dcfa54f39fcc93456c1c2c
Andrew Birt <abirt1986@gmail.com>
2022-04-08 17:58:26 -0400

    add route for inquire details

--- CoderCave/client/src/components/ApplicationViews.js
+++ CoderCave/client/src/components/ApplicationViews.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Login from "./auth/Login";
 import Register from "./auth/Register";
+import InquireDetails from "./inquire/InquireDetails";
 import InquireList from "./inquire/InquireList";
 import Main from "./main/Main";
 import TagDetails from "./tag/TagDetails";
@@ -10,6 +11,8 @@ export default function ApplicationViews({ isLoggedIn }) {
   return (
     <Routes>
       <Route path="/" element={<Main isLoggedIn={isLoggedIn} />} />
+
+      <Route path="/inquire/details/:id" element={<InquireDetails />} />
 
       <Route path="/tag/details/:id" element={<TagDetails />} />
 
