2eca7949dcb0235f77d4627768215c41de56673a
Andrew Birt <abirt1986@gmail.com>
2022-04-08 17:56:28 -0400

    create model for an inquire's comment

---
+++ CoderCave/Models/InquireComment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CoderCave.Models
+{
+    public class InquireComment
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string AuthorName { get; set; }
+        public string AuthorImageURL { get; set; }
+        public string Content { get; set; }
+        public int InquireId { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
