a4dc4e2680c352878aaae3c588b6cc513049014c
Andrew Birt <abirt1986@gmail.com>
2022-04-08 17:56:06 -0400

    create model for an answer's comment

---
+++ CoderCave/Models/AnswerComment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CoderCave.Models
+{
+    public class AnswerComment
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string AuthorName { get; set; }
+        public string AuthorImageURL { get; set; }
+        public string Content { get; set; }
+        public int AnswerId { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
