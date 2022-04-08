401288e99a39b9dd655519ca8c8f7002a489b1ae
Andrew Birt <abirt1986@gmail.com>
2022-04-08 17:49:30 -0400

    add fields for creating an Answer object

--- CoderCave/Models/Answer.cs
+++ CoderCave/Models/Answer.cs
@@ -1,6 +1,21 @@
-﻿namespace CoderCave.Models
+﻿using System;
+using System.Collections.Generic;
+
+namespace CoderCave.Models
 {
     public class Answer
     {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int InquireId { get; set; }
+        public User User { get; set; }
+        public string AuthorName { get; set; }
+        public string AuthorImageURL { get; set; }
+        public string Content { get; set; }
+        public bool IsSelected { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public List<AnswerComment> Comments { get; set; } = new List<AnswerComment>();
+        public int VotesCount { get; set; }
+        public int Score { get; set; }
     }
 }
