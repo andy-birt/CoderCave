d67eb1bcd87dce37be688bd379a3f720b277ffc4
Andrew Birt <abirt1986@gmail.com>
2022-04-09 18:39:56 -0400

    add more details to the component... a lot more details

--- CoderCave/client/src/components/inquire/InquireDetails.js
+++ CoderCave/client/src/components/inquire/InquireDetails.js
@@ -1,6 +1,10 @@
 import { useContext, useEffect } from "react";
 import { useParams } from "react-router";
 import { InquireContext } from "../../providers/InquireProvider";
+import Score from "../misc/Score";
+import CommentList from "../comment/CommentList";
+import { Button } from "reactstrap";
+import AnswerList from "../answer/AnswerList";
 
 const InquireDetails = () => {
 
@@ -13,7 +17,22 @@ const InquireDetails = () => {
   }, [id]);
 
   return (
-    <div>{inquire.title}</div>
+    <>
+      <h2 className="inquire-title">{inquire.title}</h2>
+      <div className="row">
+        <div className="col-1 mt-3">
+          <Score score={inquire.score} />
+        </div>
+        <div className="col-11">{inquire.content}</div>
+        <div className="d-flex justify-content-end align-items-center inquire-author-info">
+          <img src={inquire.authorImageURL} alt="" style={{width: 75}} />
+          <div>- {inquire.authorName} {new Date(inquire.createdAt).toLocaleString()}</div>
+        </div>
+        <CommentList comments={inquire.comments} />
+      </div>
+      <Button>Add Comment</Button>
+      <AnswerList answers={inquire.answers} />
+    </>
   );
 };
 
