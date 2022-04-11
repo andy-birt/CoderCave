505bc8e64414d1a7d151379f401ba2c32ca0ffa6
Andrew Birt <abirt1986@gmail.com>
2022-04-11 11:51:28 -0400

    refactor answer list component

--- CoderCave/client/src/components/answer/AnswerList.js
+++ CoderCave/client/src/components/answer/AnswerList.js
@@ -1,23 +1,29 @@
 import Score from "../misc/Score";
 import CommentList from "../comment/CommentList";
 import { Button } from "reactstrap";
+import { Link } from "react-router-dom";
 
 const AnswerList = ({ answers }) => {
   return answers?.map(a => {
     return (
-      <div className="mt-5">
+      <div key={a.id} className="mt-5">
         <div className="row">
-          <div className="col-1 mt-3">
+          <div className="col-1">
             <Score score={a.score} isSelected={a.isSelected} />
           </div>
-          <div className="col-10">{a.content}</div>
-          <div className="d-flex justify-content-end align-items-center answer-author-info">
-            <img src={a.authorImageURL} alt="" style={{width: 75}} />
-            <div>- {a.authorName} {new Date(a.createdAt).toLocaleString()}</div>
+          <div className="col-11">
+            <div>{a.content}</div>
+            <div className="d-flex justify-content-end align-items-center answer-author-info">
+              <Link to={`/user/${a.userId}`}>
+                <img src={a.authorImageURL} alt="" style={{width: 75}} />
+                - {a.authorName} 
+              </Link>
+              &nbsp;{new Date(a.createdAt).toLocaleString()}
+            </div>
+            <CommentList comments={a.comments} />
+            <Button>Add Comment</Button>
           </div>
-          <CommentList comments={a.comments} />
         </div>
-        <Button>Add Comment</Button>
       </div>
     );
     }
