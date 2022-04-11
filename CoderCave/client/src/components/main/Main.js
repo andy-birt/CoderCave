723c44a0ced0f60437e0e87c57c8c441902cc4ad
Andrew Birt <abirt1986@gmail.com>
2022-04-11 13:40:08 -0400

    refactor search bar

--- CoderCave/client/src/components/main/Main.js
+++ CoderCave/client/src/components/main/Main.js
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect } from "react";
-import { Container, Input } from "reactstrap";
+import { Container, Input, InputGroup, InputGroupText } from "reactstrap";
 import { TagContext } from "../../providers/TagProvider";
 import Tag from "../tag/Tag";
 import H1Logo from "../logos/H1Logo";
@@ -15,7 +15,10 @@ const Main = ({ isLoggedIn }) => {
   return (
     <Container>
       <H1Logo />
-      <Input className="main-search" placeholder="Start your search here..." />
+      <div className="search-container">
+        <span className="search-icon-main"><i className="bi bi-search"></i></span>
+        <Input className="main-search" placeholder="Start your search here..." />
+      </div>
       <div className="or-separator text-center">
         <div className="or-separator-line"></div>
         OR
