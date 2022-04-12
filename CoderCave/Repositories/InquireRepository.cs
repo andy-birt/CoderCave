982e22009bb0f0d3968699b556ac4d2573596f54
Andrew Birt <abirt1986@gmail.com>
2022-04-12 16:18:38 -0400

    run another query and return a result object

--- CoderCave/Repositories/InquireRepository.cs
+++ CoderCave/Repositories/InquireRepository.cs
@@ -10,6 +10,76 @@ namespace CoderCave.Repositories
     {
         public InquireRepository(IConfiguration configuration) : base(configuration) { }
 
+        public Result Search(string q, int p, int l)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+
+                    cmd.CommandText = $@"
+                        SELECT i.Id AS InquireId, i.UserId AS InquireUserId, i.Title, i.Content AS InquireContent, 
+                               CAST(i.Content AS NVARCHAR(255)) AS InquireContentSummary, i.CreatedAt AS InquireCreationDate,
+                               iu.DisplayName AS InquireUserDisplayName
+                        FROM Inquire i
+                            LEFT JOIN [User] iu ON i.UserId = iu.Id
+                        WHERE i.Title LIKE @q OR i.Content LIKE @q
+                        ORDER BY i.CreatedAt DESC
+                            OFFSET {(p * l - l)} ROWS
+                            FETCH NEXT {l} ROWS ONLY
+                    ";
+
+                    DbUtils.AddParameter(cmd, "@q", $"%{q}%");
+                    DbUtils.AddParameter(cmd, "@l", l);
+
+                    var reader = cmd.ExecuteReader();
+
+                    Inquire inquire = null;
+
+                    var inquiries = new List<Inquire>();
+
+                    while (reader.Read())
+                    {
+
+                        if (!DbUtils.IsDbNull(reader, "InquireId") && !inquiries.Exists(i => i.Id == DbUtils.GetInt(reader, "InquireId")))
+                        {
+                            inquire = NewSearchResultFromReader(reader);
+
+                            inquiries.Add(inquire);
+                        }
+                    }
+
+                    reader.Close();
+
+                    cmd.CommandText = $@"
+                        SELECT COUNT(*) AS ResultsCount 
+                            FROM Inquire i
+                        WHERE i.Title LIKE @q1 OR i.Content LIKE @q1
+                    ";
+
+                    DbUtils.AddParameter(cmd, "@q1", $"%{q}%");
+                    reader = cmd.ExecuteReader();
+
+                    Result result = null;
+
+                    if (reader.Read())
+                    {
+                        result = new Result()
+                        {
+                            Data = inquiries,
+                            Count = DbUtils.GetInt(reader, "ResultsCount"),
+                            StartValue = p * l - 9,
+                            EndValue = p * l < DbUtils.GetInt(reader, "ResultsCount") ? p * l : DbUtils.GetInt(reader, "ResultsCount")
+                        };
+                    }
+
+                    reader.Close();
+                    return result;
+                }
+            }
+        }
+
         public List<Inquire> GetInquiriesByTag(int tagId)
         {
             using (var conn = Connection)
@@ -62,68 +132,8 @@ namespace CoderCave.Repositories
                 conn.Open();
                 using (var cmd = conn.CreateCommand())
                 {
-                    cmd.CommandText = @"
-                        SELECT t.Id AS TagId, t.[Name] AS TagName, CAST(t.Description AS NVARCHAR(MAX)) AS TagDescription,
-                               i.Id AS InquireId, i.UserId AS InquireUserId, i.Title, CAST(i.Content AS NVARCHAR(MAX)) AS InquireContent, CAST(i.Content AS NVARCHAR(255)) AS InquireContentSummary, i.CreatedAt AS InquireCreationDate,
-                               iu.DisplayName AS InquireUserDisplayName, iu.ImageURL AS InquireUserImageURL,
-                               ic.Id AS InquireCommentId, CAST(ic.Content AS NVARCHAR(MAX)) AS InquireCommentContent, ic.CreatedAt AS InquireCommentCreationDate,
-                               icu.Id AS InquireCommentUserId, icu.DisplayName AS InquireCommentUserDisplayName, icu.ImageURL AS InquireCommentUserImageURL,
-                               a.Id AS AnswerId, a.UserId AS AnswerUserId, CAST(a.Content AS NVARCHAR(MAX)) AS AnswerContent, a.CreatedAt AS AnswerCreationDate, a.IsSelected,
-                               ac.Id AS AnswerCommentId, CAST(ac.Content AS NVARCHAR(MAX)) AS AnswerCommentContent, ac.CreatedAt AS AnswerCommentCreationDate,
-                               au.DisplayName AS AnswerUserDisplayName, au.ImageURL AS AnswerUserImageURL,
-                               acu.Id AS AnswerCommentUserId, acu.DisplayName AS AnswerCommentUserDisplayName, acu.ImageURL AS AnswerCommentUserImageURL,
-                               ISNULL(vi.InquireScore, 0) AS InquireScore,
-                               ISNULL(va.AnswerScore, 0) AS AnswerScore,
-                               ISNULL(icc.InquireCommentsCount, 0) AS InquireCommentsCount,
-                               ISNULL(act.AnswersCount, 0) AS AnswersCount,
-                               ISNULL(vic.VotesCount, 0) AS InquireVotesCount,
-                               ISNULL(vac.VotesCount, 0) AS AnswerVotesCount
-                            FROM Inquire i
-                            LEFT JOIN InquireTag it ON i.Id = it.InquireId
-                            LEFT JOIN Tag t ON t.Id = it.TagId
-                            LEFT JOIN InquireComment ic ON i.Id = ic.InquireId
-                            LEFT JOIN [User] iu ON iu.Id = i.UserId 
-                            LEFT JOIN [User] icu ON icu.Id = ic.UserId
-	                        LEFT JOIN Answer a ON i.Id = a.InquireId
-                            LEFT JOIN AnswerComment ac ON a.Id = ac.AnswerId
-                            LEFT JOIN [User] au ON au.Id = a.UserId
-                            LEFT JOIN [User] acu ON acu.Id = ac.UserId
-                            LEFT JOIN
-                                (
-                                    SELECT InquireId, SUM([Value]) AS InquireScore
-                                    FROM VoteInquire
-                                    GROUP BY InquireId
-                                ) vi ON i.Id = vi.InquireId
-                            LEFT JOIN
-                                (
-                                    SELECT AnswerId, SUM([Value]) AS AnswerScore
-                                    FROM VoteAnswer
-                                    GROUP BY AnswerId
-                                ) va ON a.Id = va.AnswerId
-                            LEFT JOIN 
-                                (
-                                    SELECT InquireId, COUNT(InquireId) AS InquireCommentsCount
-                                    FROM InquireComment
-                                    GROUP BY InquireId
-                                ) icc ON icc.InquireId = i.Id
-                            LEFT JOIN
-                                (
-                                    SELECT InquireId, COUNT(InquireId) AS AnswersCount
-                                    FROM Answer
-                                    GROUP BY InquireId
-                                ) act ON act.InquireId = i.Id
-                            LEFT JOIN
-                                (
-                                    SELECT InquireId, COUNT(DISTINCT Id) AS VotesCount
-                                    FROM VoteInquire
-                                    GROUP BY InquireId
-                                ) vic ON i.Id = vic.InquireId
-                            LEFT JOIN
-                                (
-                                    SELECT AnswerId, COUNT(DISTINCT Id) AS VotesCount
-                                    FROM VoteAnswer
-                                    GROUP BY AnswerId
-                                ) vac ON a.Id = vac.AnswerId
+                    cmd.CommandText = @$"
+                        {BeefySelect()}
                         WHERE i.Id = @Id
                         ORDER BY a.IsSelected DESC, AnswerScore DESC
                     ";
@@ -188,6 +198,20 @@ namespace CoderCave.Repositories
                     return inquire;
                 }
             }
+        }
+
+        private Inquire NewSearchResultFromReader(SqlDataReader r)
+        {
+            return new Inquire()
+            {
+                Id = DbUtils.GetInt(r, "InquireId"),
+                UserId = DbUtils.GetInt(r, "InquireUserId"),
+                Title = DbUtils.GetString(r, "Title"),
+                Content = DbUtils.GetString(r, "InquireContent"),
+                ContentSummary = DbUtils.GetString(r, "InquireContentSummary"),
+                CreatedAt = DbUtils.GetDateTime(r, "InquireCreationDate"),
+                AuthorName = DbUtils.GetString(r, "InquireUserDisplayName")
+            };
         }
 
         private Inquire NewInquireFromReader(SqlDataReader r)
@@ -279,6 +303,73 @@ namespace CoderCave.Repositories
                 Name = DbUtils.GetString(r, "TagName"),
                 Description = DbUtils.GetString(r, "TagDescription")
             };
+        }
+
+        private string BeefySelect()
+        {
+            return @"
+                        SELECT t.Id AS TagId, t.[Name] AS TagName, CAST(t.Description AS NVARCHAR(MAX)) AS TagDescription,
+                               i.Id AS InquireId, i.UserId AS InquireUserId, i.Title, CAST(i.Content AS NVARCHAR(MAX)) AS InquireContent, CAST(i.Content AS NVARCHAR(255)) AS InquireContentSummary, i.CreatedAt AS InquireCreationDate,
+                               iu.DisplayName AS InquireUserDisplayName, iu.ImageURL AS InquireUserImageURL,
+                               ic.Id AS InquireCommentId, CAST(ic.Content AS NVARCHAR(MAX)) AS InquireCommentContent, ic.CreatedAt AS InquireCommentCreationDate,
+                               icu.Id AS InquireCommentUserId, icu.DisplayName AS InquireCommentUserDisplayName, icu.ImageURL AS InquireCommentUserImageURL,
+                               a.Id AS AnswerId, a.UserId AS AnswerUserId, CAST(a.Content AS NVARCHAR(MAX)) AS AnswerContent, a.CreatedAt AS AnswerCreationDate, a.IsSelected,
+                               ac.Id AS AnswerCommentId, CAST(ac.Content AS NVARCHAR(MAX)) AS AnswerCommentContent, ac.CreatedAt AS AnswerCommentCreationDate,
+                               au.DisplayName AS AnswerUserDisplayName, au.ImageURL AS AnswerUserImageURL,
+                               acu.Id AS AnswerCommentUserId, acu.DisplayName AS AnswerCommentUserDisplayName, acu.ImageURL AS AnswerCommentUserImageURL,
+                               ISNULL(vi.InquireScore, 0) AS InquireScore,
+                               ISNULL(va.AnswerScore, 0) AS AnswerScore,
+                               ISNULL(icc.InquireCommentsCount, 0) AS InquireCommentsCount,
+                               ISNULL(act.AnswersCount, 0) AS AnswersCount,
+                               ISNULL(vic.VotesCount, 0) AS InquireVotesCount,
+                               ISNULL(vac.VotesCount, 0) AS AnswerVotesCount
+                            FROM Inquire i
+                            LEFT JOIN InquireTag it ON i.Id = it.InquireId
+                            LEFT JOIN Tag t ON t.Id = it.TagId
+                            LEFT JOIN InquireComment ic ON i.Id = ic.InquireId
+                            LEFT JOIN [User] iu ON iu.Id = i.UserId 
+                            LEFT JOIN [User] icu ON icu.Id = ic.UserId
+	                        LEFT JOIN Answer a ON i.Id = a.InquireId
+                            LEFT JOIN AnswerComment ac ON a.Id = ac.AnswerId
+                            LEFT JOIN [User] au ON au.Id = a.UserId
+                            LEFT JOIN [User] acu ON acu.Id = ac.UserId
+                            LEFT JOIN
+                                (
+                                    SELECT InquireId, SUM([Value]) AS InquireScore
+                                    FROM VoteInquire
+                                    GROUP BY InquireId
+                                ) vi ON i.Id = vi.InquireId
+                            LEFT JOIN
+                                (
+                                    SELECT AnswerId, SUM([Value]) AS AnswerScore
+                                    FROM VoteAnswer
+                                    GROUP BY AnswerId
+                                ) va ON a.Id = va.AnswerId
+                            LEFT JOIN 
+                                (
+                                    SELECT InquireId, COUNT(InquireId) AS InquireCommentsCount
+                                    FROM InquireComment
+                                    GROUP BY InquireId
+                                ) icc ON icc.InquireId = i.Id
+                            LEFT JOIN
+                                (
+                                    SELECT InquireId, COUNT(InquireId) AS AnswersCount
+                                    FROM Answer
+                                    GROUP BY InquireId
+                                ) act ON act.InquireId = i.Id
+                            LEFT JOIN
+                                (
+                                    SELECT InquireId, COUNT(DISTINCT Id) AS VotesCount
+                                    FROM VoteInquire
+                                    GROUP BY InquireId
+                                ) vic ON i.Id = vic.InquireId
+                            LEFT JOIN
+                                (
+                                    SELECT AnswerId, COUNT(DISTINCT Id) AS VotesCount
+                                    FROM VoteAnswer
+                                    GROUP BY AnswerId
+                                ) vac ON a.Id = vac.AnswerId
+                    ";
         }
     }
 }
