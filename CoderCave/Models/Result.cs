4223fdea7bd2e0007c0cf70d1ce281ab3ee667c9
Andrew Birt <abirt1986@gmail.com>
2022-04-12 16:17:53 -0400

    create a result object for inquire search data

---
+++ CoderCave/Models/Result.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace CoderCave.Models
+{
+    public class Result
+    {
+        public List<Inquire> Data { get; set; }
+        public int Count { get; set; }
+        public int StartValue { get; set; }
+        public int EndValue { get; set; }
+    }
+}
