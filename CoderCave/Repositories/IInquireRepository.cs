acc0342c9d8944e4090c08ef4bf5d443eee6bd82
Andrew Birt <abirt1986@gmail.com>
2022-04-13 21:45:45 -0400

    add post method to interface

--- CoderCave/Repositories/IInquireRepository.cs
+++ CoderCave/Repositories/IInquireRepository.cs
@@ -5,6 +5,7 @@ namespace CoderCave.Repositories
 {
     public interface IInquireRepository
     {
+        void Add(Inquire inquire);
         Inquire GetInquirie(int id);
         List<Inquire> GetInquiriesByTag(int tagId);
         Result Search(string q, int p, int l);
