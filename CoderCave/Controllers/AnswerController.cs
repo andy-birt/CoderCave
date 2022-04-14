db82f2b975a96142609463a57b2a2e77af5f1ad1
Andrew Birt <abirt1986@gmail.com>
2022-04-14 15:46:40 -0400

    create answer controller

---
+++ CoderCave/Controllers/AnswerController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using CoderCave.Models;
+using CoderCave.Repositories;
+using System;
+
+namespace CoderCave.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AnswerController : ControllerBase
+    {
+        private readonly IAnswerRepository _answerRepository;
+
+        public AnswerController(IAnswerRepository answerRepository)
+        {
+            _answerRepository = answerRepository;
+        }
+
+        [HttpPost]
+        public IActionResult Post(Answer answer)
+        {
+            answer.IsSelected = false;
+            answer.CreatedAt = DateTime.Now;
+            _answerRepository.Add(answer);
+            return CreatedAtAction(nameof(Answer), new { id = answer.Id }, answer);
+        }
+    }
+}
