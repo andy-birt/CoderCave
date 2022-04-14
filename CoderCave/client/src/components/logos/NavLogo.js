c9cad50746a8ef54dbeef1d3b148cc1d26b87bb1
Andrew Birt <abirt1986@gmail.com>
2022-04-13 21:49:50 -0400

    create logo for nav bar

---
+++ CoderCave/client/src/components/logos/NavLogo.js
@@ -0,0 +1,23 @@
+const NavLogo = () => {
+  return (
+    <span 
+      style={{
+        fontFamily: "'Josefin Sans', sans-serif",
+        fontSize: "2rem"
+      }} 
+      >
+      <span className="blue">C</span>
+      <span className="red">o</span>
+      <span className="yellow">d</span>
+      <span className="red">e</span>
+      <span className="blue">r</span>
+      &nbsp;
+      <span className="green">C</span>
+      <span className="red">a</span>
+      <span className="blue">v</span>
+      <span className="yellow">e</span>
+    </span>
+  );
+};
+
+export default NavLogo;
