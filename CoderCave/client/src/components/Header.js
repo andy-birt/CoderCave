950287f3d80686fc3b7e1758cdaf378057f699ec
Andrew Birt <abirt1986@gmail.com>
2022-04-13 21:48:37 -0400

    create header component

---
+++ CoderCave/client/src/components/Header.js
@@ -0,0 +1,62 @@
+import { Collapse, Nav, Navbar, NavbarBrand,  NavbarToggler, NavItem, NavLink } from "reactstrap";
+import NavLogo from "./logos/NavLogo";
+import { logout } from "../managers/authManager";
+import { useNavigate } from "react-router";
+
+
+const Header = ({ isLoggedIn }) => {
+
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    logout();
+    navigate("/");
+  };
+
+  return ( 
+    <Navbar
+      color="light"
+      expand="md"
+      fixed=""
+      light
+    >
+      <NavbarBrand href="/">
+        <NavLogo />
+      </NavbarBrand>
+      <NavbarToggler onClick={function noRefCheck(){}} />
+      <Collapse navbar>
+        <Nav
+          className="me-auto"
+          navbar
+        >
+          { !isLoggedIn &&
+            <>
+              <NavItem>
+                <NavLink href="/login">
+                  Login
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/register">
+                  Register
+                </NavLink>
+              </NavItem>
+            </>
+          }
+
+          { isLoggedIn &&
+            <>
+              <NavItem>
+                <NavLink onClick={handleLogout} href="#">
+                  Logout
+                </NavLink>
+              </NavItem>
+            </>
+          }
+        </Nav>
+      </Collapse>
+    </Navbar>
+  );
+};
+
+export default Header;
