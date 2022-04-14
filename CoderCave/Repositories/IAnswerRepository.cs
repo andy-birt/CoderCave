7b29592cffe02821196117adaae3b59f3b5ce4fd
Andrew Birt <abirt1986@gmail.com>
2022-04-14 17:15:48 -0400

    implement GetAnswer in interface

--- CoderCave/Repositories/IAnswerRepository.cs
+++ CoderCave/Repositories/IAnswerRepository.cs
@@ -6,6 +6,7 @@ namespace CoderCave.Repositories
     {
         void Add(Answer answer);
         void Delete(int id);
+        Answer GetAnswer(int id);
         void Update(Answer answer);
     }
 }
