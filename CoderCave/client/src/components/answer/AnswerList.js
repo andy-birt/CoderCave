2321f066c758179f45870e49dd95f949231cd7bd
Andrew Birt <abirt1986@gmail.com>
2022-04-14 17:21:00 -0400

    answer list now has options for each answer depending on the current user

--- CoderCave/client/src/components/answer/AnswerList.js
+++ CoderCave/client/src/components/answer/AnswerList.js
@@ -1,9 +1,32 @@
 import Score from "../misc/Score";
 import CommentList from "../comment/CommentList";
 import { Button } from "reactstrap";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { getAuth } from "firebase/auth";
+import { useEffect, useState } from "react";
 
-const AnswerList = ({ answers }) => {
+const AnswerList = ({ answers, isLoggedIn }) => {
+
+  let loggedInUserFBID = null;
+   
+  if (isLoggedIn) loggedInUserFBID = getAuth().currentUser.uid;
+
+  const [currentUser, setCurrentUser] = useState({});
+
+  const navigate = useNavigate();
+
+  const deleteAnswer = () => {
+
+  };
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      fetch(`/api/user/${loggedInUserFBID}`)
+        .then(r => r.json())
+        .then(setCurrentUser);
+    }
+  }, [currentUser.id]);
+
   return answers?.map(a => {
     return (
       <div key={a.id} className="mt-5">
@@ -14,6 +37,14 @@ const AnswerList = ({ answers }) => {
           <div className="col-11">
             <div>{a.content}</div>
             <div className="d-flex justify-content-end align-items-center answer-author-info">
+            {
+              (isLoggedIn && a.userId === currentUser.id ) &&
+              <>
+                <Button onClick={() => navigate(`/inquire/answer/edit/${a.id}`)} className="mb-3">Edit Answer</Button>
+                {' '}
+                <Button onClick={() => deleteAnswer()} className="mb-3">Delete Answer</Button>
+              </>
+            }
               <Link to={`/user/${a.userId}`}>
                 <img src={a.authorImageURL} alt="" style={{width: 75}} />
                 - {a.authorName} 
