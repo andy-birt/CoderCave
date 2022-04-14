cba9cb674f2fabe5cd348a1e7f949cf438f0f3f1
Andrew Birt <abirt1986@gmail.com>
2022-04-14 17:17:28 -0400

    create getAnswerById provider function

--- CoderCave/client/src/providers/AnswerProvider.js
+++ CoderCave/client/src/providers/AnswerProvider.js
@@ -4,6 +4,11 @@ export const AnswerContext = createContext();
 
 export const AnswerProvider = (props) => {
 
+  const getAnswerById = (id) => {
+    return fetch(`/api/answer/${id}`)
+      .then(r => r.json());
+  };
+
   const saveAnswer = (answer) => {
     return fetch('/api/answer', {
       method: "POST",
@@ -11,7 +16,7 @@ export const AnswerProvider = (props) => {
         "Content-Type": "application/json"
       },
       body: JSON.stringify(answer)
-    })
+    });
   };
 
   const editAnswer = (answer) => {
@@ -21,18 +26,18 @@ export const AnswerProvider = (props) => {
         "Content-Type": "application/json"
       },
       body: JSON.stringify(answer)
-    })
+    });
   };
 
   const deleteAnswer = (answerId) => {
     return fetch(`/api/answer/${answerId}`, {
       method: "DELETE"
-    })
+    });
   };
 
   return (
     <AnswerContext.Provider value={{
-      saveAnswer, editAnswer, deleteAnswer
+      getAnswerById, saveAnswer, editAnswer, deleteAnswer
     }}>
       {props.children}
     </AnswerContext.Provider>
