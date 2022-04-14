637b2d432ca2d0b9018690a2dc5dc4c7e27f046d
Andrew Birt <abirt1986@gmail.com>
2022-04-14 15:43:15 -0400

    create answer provider for answer crud

---
+++ CoderCave/client/src/providers/AnswerProvider.js
@@ -0,0 +1,40 @@
+import { createContext } from "react";
+
+export const AnswerContext = createContext();
+
+export const AnswerProvider = (props) => {
+
+  const saveAnswer = (answer) => {
+    return fetch('/api/answer', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(answer)
+    })
+  };
+
+  const editAnswer = (answer) => {
+    return fetch(`/api/answer/${answer.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(answer)
+    })
+  };
+
+  const deleteAnswer = (answerId) => {
+    return fetch(`/api/answer/${answerId}`, {
+      method: "DELETE"
+    })
+  };
+
+  return (
+    <AnswerContext.Provider value={{
+      saveAnswer, editAnswer, deleteAnswer
+    }}>
+      {props.children}
+    </AnswerContext.Provider>
+  );
+};
