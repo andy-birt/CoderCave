95f5d126718182939a7bfd278bbefde772e67d66
Andrew Birt <abirt1986@gmail.com>
2022-04-14 15:47:03 -0400

    create answer interface

---
+++ CoderCave/Repositories/IAnswerRepository.cs
@@ -0,0 +1,11 @@
+﻿using CoderCave.Models;
+
+namespace CoderCave.Repositories
+{
+    public interface IAnswerRepository
+    {
+        void Add(Answer answer);
+        void Delete(int id);
+        void Update(Answer answer);
+    }
+}
