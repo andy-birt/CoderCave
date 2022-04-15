3abf97b126a80a84f94f033cf74b5b0c65c08367
Andrew Birt <abirt1986@gmail.com>
2022-04-15 08:49:32 -0400

    create AnswerComment component

---
+++ CoderCave/client/src/components/comment/AnswerCommentForm.js
@@ -0,0 +1,75 @@
+import { getAuth } from "firebase/auth";
+import { useContext, useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router";
+import { Button, Container, Form, FormGroup, FormText, Input, Label } from "reactstrap";
+import { AnswerContext } from "../../providers/AnswerProvider";
+import { CommentContext } from "../../providers/CommentProvider";
+
+const AnswerCommentForm = ({ isLoggedIn }) => {
+
+  const currentUser = getAuth().currentUser;
+
+  const { answer, getAnswerById } = useContext(AnswerContext);
+
+  const { saveAnswerComment, editAnswerComment, getAnswerCommentById } = useContext(CommentContext);
+
+  const [ comment, setComment ] = useState({ answerId: answer.id });
+
+  const { id } = useParams();
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!comment.id) {
+      saveAnswerComment(comment)
+        .then(() => navigate(`/inquire/details/${answer.inquireId}`));
+    } else {
+      editAnswerComment(comment)
+        .then(() => navigate(`/inquire/details/${answer.inquireId}`));
+    }
+  };
+
+  const handleChange = (e) => {
+    const newComment = { ...comment };
+    newComment[e.target.id] = e.target.value;    
+    setComment(newComment);
+  };
+
+  useEffect(() => {
+    if (!comment.inquireId) {
+      getAnswerById(id);
+    } 
+    
+    if (isLoggedIn) {
+      fetch(`/api/user/${currentUser.uid}`)
+        .then(r => r.json())
+        .then((user) => setComment({...comment, userId: user.id}));
+    } else {
+      navigate(-1);
+    }
+
+    if (id !== answer.id) {
+      getAnswerCommentById(id)
+        .then(setComment);
+    } 
+  }, [id]);
+
+  return (
+    <Container>
+      <Form onSubmit={handleSubmit}>
+        <Button color="dark" className="mt-3 mb-3" outline onClick={() => navigate(-1)} >Back</Button>
+        <h3>{answer.title}</h3>
+        <FormText>{answer.content}</FormText>
+        <p></p>
+        <FormGroup>
+          <Label>Content</Label>
+          <Input onChange={handleChange} defaultValue={comment.content} id="content" type="textarea" placeholder="What do you think about this question?" />
+        </FormGroup>
+        <Button type="submit" >Submit</Button>
+      </Form>
+    </Container>
+  );
+};
+
+export default AnswerCommentForm;
