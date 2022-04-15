a9681044ac32d1729f172500dcab9cdfc385f572
Andrew Birt <abirt1986@gmail.com>
2022-04-14 21:23:09 -0400

    create comment controller for inquires

---
+++ CoderCave/Controllers/InquireCommentController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using CoderCave.Models;
+using CoderCave.Repositories;
+using System;
+
+namespace CoderCave.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InquireCommentController : ControllerBase
+    {
+        private readonly IInquireCommentRepository _commentRepository;
+
+        public InquireCommentController(IInquireCommentRepository commentRepository)
+        {
+            _commentRepository = commentRepository;
+        }
+
+        [HttpPost]
+        public IActionResult Post(InquireComment comment)
+        {
+            comment.CreatedAt = DateTime.Now;
+            _commentRepository.Add(comment);
+            return NoContent();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, InquireComment comment)
+        {
+            if (id != comment.Id)
+            {
+                return BadRequest();
+            }
+            _commentRepository.Update(comment);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _commentRepository.Delete(id);
+            return NoContent();
+        }
+    }
+}
