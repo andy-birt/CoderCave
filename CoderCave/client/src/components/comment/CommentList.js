d3d3fc8f3613291b9427db8e223198006eb28537
Andrew Birt <abirt1986@gmail.com>
2022-04-14 22:02:09 -0400

    refactor CommentList to be more like AnswerList

--- CoderCave/client/src/components/comment/CommentList.js
+++ CoderCave/client/src/components/comment/CommentList.js
@@ -1,14 +1,54 @@
-import { Link } from "react-router-dom";
-import { Card, CardBody } from "reactstrap";
+import { getAuth } from "firebase/auth";
+import { useContext, useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Button, Card, CardBody } from "reactstrap";
+import { CommentContext } from "../../providers/CommentProvider";
 
-const CommentList = ({ comments }) => {
+const CommentList = ({ comments, isLoggedIn, getInquire }) => {
+
+  let loggedInUserFBID = null;
+   
+  if (isLoggedIn) loggedInUserFBID = getAuth().currentUser.uid;
+
+  const [currentUser, setCurrentUser] = useState({});
+
+  const { deleteInquireComment } = useContext(CommentContext);
+
+  const navigate = useNavigate();
+
+  const handleDeleteComment = (id, inquireId) => {
+    if (window.confirm('Are you sure you want to delete this answer?')) {
+      deleteInquireComment(id)
+        .then(() => getInquire(inquireId));
+    }
+  };
  
+  useEffect(() => {
+    if (isLoggedIn) {
+      fetch(`/api/user/${loggedInUserFBID}`)
+        .then(r => r.json())
+        .then(setCurrentUser);
+    }
+  }, [currentUser.id]);
+
   return comments?.map(c => {
     return (
       <Card key={c.id}>
         <CardBody>
           <div className="mr-2"><small>{c.content} </small> </div>
-          <div className="d-flex justify-content-end align-items-center comment-author-info"><Link to={`/user/${c.userId}`}><img src={c.authorImageURL} alt="" className="comment-avatar" /> - {c.authorName}</Link> {new Date(c.createdAt).toLocaleString()}</div>
+          <div className="d-flex justify-content-end align-items-center comment-author-info">
+            {
+              (isLoggedIn && c.userId === currentUser.id ) &&
+              <>
+                <Button onClick={() => navigate(`/inquire/comment/edit/${c.id}`)} className="mb-3" style={{marginRight: ".25rem"}}>Edit Comment</Button>
+                <Button onClick={() => handleDeleteComment(c.id, c.inquireId)} className="mb-3">Delete Comment</Button>
+              </>
+            }
+            <Link to={`/user/${c.userId}`}><img src={c.authorImageURL} alt="" className="comment-avatar" />
+              - {c.authorName}
+            </Link> 
+            {new Date(c.createdAt).toLocaleString()}
+          </div>
         </CardBody>
       </Card>
     );
