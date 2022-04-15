9218732b881ca73a28789a86847f102883d64511
Andrew Birt <abirt1986@gmail.com>
2022-04-15 16:28:27 -0400

    working currentuser options for inquire and answer comments

--- CoderCave/client/src/components/comment/CommentList.js
+++ CoderCave/client/src/components/comment/CommentList.js
@@ -3,6 +3,7 @@ import { useContext, useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Button, Card, CardBody } from "reactstrap";
 import { CommentContext } from "../../providers/CommentProvider";
+import { InquireContext } from "../../providers/InquireProvider";
 
 const CommentList = ({ comments, isLoggedIn, getInquire }) => {
 
@@ -12,14 +13,21 @@ const CommentList = ({ comments, isLoggedIn, getInquire }) => {
 
   const [currentUser, setCurrentUser] = useState({});
 
-  const { deleteInquireComment } = useContext(CommentContext);
+  const { deleteInquireComment, deleteAnswerComment } = useContext(CommentContext);
+
+  const { inquire } = useContext(InquireContext);
 
   const navigate = useNavigate();
 
-  const handleDeleteComment = (id, inquireId) => {
-    if (window.confirm('Are you sure you want to delete this answer?')) {
-      deleteInquireComment(id)
-        .then(() => getInquire(inquireId));
+  const handleDeleteComment = (id, comment) => {
+    if (window.confirm('Are you sure you want to delete this comment?')) {
+      if (comment.inquireId) {
+        deleteInquireComment(id)
+          .then(() => getInquire(comment.inquireId));
+      } else {
+        deleteAnswerComment(id)
+          .then(() => getInquire(inquire.id));
+      }
     }
   };
  
@@ -31,6 +39,8 @@ const CommentList = ({ comments, isLoggedIn, getInquire }) => {
     }
   }, [currentUser.id]);
 
+  
+
   return comments?.map(c => {
     return (
       <Card key={c.id}>
@@ -38,10 +48,17 @@ const CommentList = ({ comments, isLoggedIn, getInquire }) => {
           <div className="mr-2"><small>{c.content} </small> </div>
           <div className="d-flex justify-content-end align-items-center comment-author-info">
             {
-              (isLoggedIn && c.userId === currentUser.id ) &&
+              (isLoggedIn && c.inquireId && (c.userId === currentUser.id )) &&
               <>
                 <Button onClick={() => navigate(`/inquire/comment/edit/${c.id}`)} className="mb-3" style={{marginRight: ".25rem"}}>Edit Comment</Button>
-                <Button onClick={() => handleDeleteComment(c.id, c.inquireId)} className="mb-3">Delete Comment</Button>
+                <Button onClick={() => handleDeleteComment(c.id, c)} className="mb-3">Delete Comment</Button>
+              </>
+            }
+            {
+              (isLoggedIn && c.answerId && (c.userId === currentUser.id )) &&
+              <>
+                <Button onClick={() => navigate(`/inquire/answer/comment/edit/${c.id}`)} className="mb-3" style={{marginRight: ".25rem"}}>Edit Comment</Button>
+                <Button onClick={() => handleDeleteComment(c.id, c)} className="mb-3">Delete Comment</Button>
               </>
             }
             <Link to={`/user/${c.userId}`}><img src={c.authorImageURL} alt="" className="comment-avatar" />
