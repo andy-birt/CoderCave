722c0fa287aa88fbdbd8920f64b108a5162c9939
Andrew Birt <abirt1986@gmail.com>
2022-04-26 14:02:42 -0400

    conditionally render the inquire details component

--- CoderCave/client/src/components/inquire/InquireDetails.js
+++ CoderCave/client/src/components/inquire/InquireDetails.js
@@ -60,48 +60,58 @@ const InquireDetails = ({ isLoggedIn }) => {
     }
   }, [currentUser.id]);
 
-  return (
-    <>
-      <Button color="dark" className="mt-3" outline onClick={() => navigate(-1)} >Back</Button>
-      <h2 className="inquire-title">{inquire.title}</h2>
-      <Button onClick={handleAnswerButtonClick}  className="mb-3">Add Answer</Button>
-      {' '}
-      {
-        (isLoggedIn && (inquire.userId === currentUser.id || currentUser.userType?.type === "Admin")) &&
-        <>
-          <Button onClick={() => navigate(`/inquire/edit/${id}`)} className="mb-3">Edit Question</Button>
-          {' '}
-          <Button onClick={() => archiveInquire()} className="mb-3">Archive Question</Button>
-        </>
-      }
-      <div className="row">
-        <div className="col-1">
-          <Score voteType={'inquire'}  score={inquire.score} id={inquire.id} />
-        </div>
-        <div className="col-11">
-          <ReactMarkdown
-            children={inquire.content}
-            rehypePlugins={[rehypeSanitize]}
-          />
-          <div className="d-flex justify-content-end align-items-center inquire-author-info">
-            <div  className="details-tag-list mt-2">
-              <TagList tags={inquire.tags} />
-            </div>
-            <Link to={`/user/details/${inquire.userId}`}>
-              <img src={inquire.authorImageURL} alt="" style={{width: 75}} />
-              - {inquire.authorName} 
-            </Link>
-            {new Date(inquire.createdAt).toLocaleString()}  
+  if ( !inquire.isArchived || currentUser.id === inquire.userId || currentUser.userType?.type === "Admin" ) {
+
+    return (
+      <>
+        <Button color="dark" className="mt-3" outline onClick={() => navigate(-1)} >Back</Button>
+        <h2 className="inquire-title">{inquire.title}</h2>
+        <Button onClick={handleAnswerButtonClick}  className="mb-3">Add Answer</Button>
+        {' '}
+        {
+          (isLoggedIn && (inquire.userId === currentUser.id || currentUser.userType?.type === "Admin")) &&
+          <>
+            <Button onClick={() => navigate(`/inquire/edit/${id}`)} className="mb-3">Edit Question</Button>
+            {' '}
+            <Button onClick={() => archiveInquire()} className="mb-3">Archive Question</Button>
+          </>
+        }
+        <div className="row">
+          <div className="col-1">
+            <Score voteType={'inquire'}  score={inquire.score} id={inquire.id} />
           </div>
-          <Container className="comments-container" >
-            <CommentList comments={inquire.comments} isLoggedIn={isLoggedIn} getInquire={getInquire} />
-            <Button onClick={handleCommentButtonClick} >Add Comment</Button>
-          </Container>
+          <div className="col-11">
+            <ReactMarkdown
+              children={inquire.content}
+              rehypePlugins={[rehypeSanitize]}
+            />
+            <div className="d-flex justify-content-end align-items-center inquire-author-info">
+              <div  className="details-tag-list mt-2">
+                <TagList tags={inquire.tags} />
+              </div>
+              <Link to={`/user/details/${inquire.userId}`}>
+                <img src={inquire.authorImageURL} alt="" style={{width: 75}} />
+                - {inquire.authorName} 
+              </Link>
+              {new Date(inquire.createdAt).toLocaleString()}  
+            </div>
+            <Container className="comments-container" >
+              <CommentList comments={inquire.comments} isLoggedIn={isLoggedIn} getInquire={getInquire} />
+              <Button onClick={handleCommentButtonClick} >Add Comment</Button>
+            </Container>
+          </div>
         </div>
-      </div>
-      <AnswerList inquireUserId={inquire.userId} answers={inquire.answers} isLoggedIn={isLoggedIn} getInquire={getInquire} />
-    </>
-  );
+        <AnswerList inquireUserId={inquire.userId} answers={inquire.answers} isLoggedIn={isLoggedIn} getInquire={getInquire} />
+      </>
+    );
+
+  } else {
+
+    return (
+      <>Question not Found</>
+    );
+
+  }
 };
 
 export default InquireDetails;
